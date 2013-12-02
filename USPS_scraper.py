8189dca669da87398b16839e69404213ab3696c0
Navin <navin.kurupacheril@gmail.com>
2013-12-01 19:10:38 -0800

    It works!

--- USPS_scraper.py
+++ USPS_scraper.py
@@ -6,16 +6,26 @@ from itertools import chain
 removeNonAscii = lambda s: "".join(i for i in s if ord(i)<128)
 
 url = "https://tools.usps.com/go/TrackConfirmAction.action"
+#url = "https://google.com"
 _params = dict(tRef="fullpage", tLc="2")
+_headers = {'User-Agent' : r"""Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36"""}
 
 def track(numbers):
 	params = dict(chain( _params.items(), {"tLabels" : ','.join(numbers)}.items() ))
-	r = requests.get(url, params=params)
+	r = requests.get(url, params=params, headers=_headers)
 	html_text = removeNonAscii(r.text)
-	print(r.url)
+	for req in r.history:
+		print(req.url)
 	tree = lxml.html.fromstring(html_text)
 	#print(tree)
-	status = tree.xpath(r"""/html/body""")
+	status_list = []
+	i=1
+	while True:
+		try:
+			status_list.append(tree.xpath(r"""//*[@id="results-multi"]/div[%d]/div/div[1]/div[2]/div[1]/h2/text()"""%i)[0])
+		except IndexError:
+			break
+		i+=1
 	#print(etree)
 	#print(etree.tostring(tree, pretty_print=True).strip())
-	return status
+	return status_list
--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 from USPS_scraper import track
 
+tracking_pattern = "LN1478025%02dCN"
 if __name__ == '__main__':
-	numbers = ["9400110200882950235373", "LN147802515CN"]
-	tracking_info = track(numbers)
-	print(tracking_info)
+	#numbers = ["9400110200882950235373", "LN147802515CN"]
+	for i in range(0,100,10):
+		numbers = [tracking_pattern%i for i in range(15+i,25+i)]
+		tracking_info = track(numbers)
+		for i in range(15+i,25+i):
+			info = tracking_info[i%10]
+			if info!="error":
+				print(tracking_pattern%i, info)
