810c0a9a740b4a204741cc4a4d0362f730819b0f
Navin <navin.kurupacheril@gmail.com>
2013-12-03 00:18:17 -0800

    Added concurrency to test.py
    still breaks after a few requests...

--- test.py
+++ test.py
@@ -1,24 +1,35 @@
 from USPS_scraper import track
+from concurrent.futures import ProcessPoolExecutor, as_completed
+import sys
 
 import sqlite3
 db = sqlite3.connect('db', isolation_level=None)
-
 tracking_pattern = "LN147802%03dCN"
 #real tracking number: LN147802515CN
 stride=10
 
-def record_info(numbers):
-	tracking_infos = track(numbers)
-	if len(tracking_infos)!=stride:
-		print("got %d tracking infos, expected %d"%(len(tracking_infos), stride))
-		return
-	for j in range(i,i+stride):
-		info = tracking_infos[j%stride]
-		tracking_info = (tracking_pattern%j, info)
-		print(tracking_info)
-		db.execute("insert into tracking_infos values (?, ?)", tracking_info)
-
+def record_info(numbers, i):
+	try:
+		tracking_infos = track(numbers)
+		if len(tracking_infos)!=stride:
+			print("got %d tracking infos, expected %d"%(len(tracking_infos), stride))
+			return
+		for j in range(i,i+stride):
+			info = tracking_infos[j%stride]
+			tracking_info = (tracking_pattern%j, info)
+			print(tracking_info)
+			db.execute("insert into tracking_infos values (?, ?)", tracking_info)
+	except Exception as e:
+		print(e)
+	sys.stdout.flush()
 if __name__ == '__main__':
+	executor = ProcessPoolExecutor(max_workers=2)
+	futures = []
+	tasks = 0
 	for i in range(0,1000,stride):
 		numbers = [tracking_pattern%j for j in range(i,i+stride)]
-		record_info(numbers)
+		futures.append(executor.submit(record_info, numbers, i))
+		tasks+=1
+	print("submitted tasks")
+	for i, future in enumerate(as_completed(futures)):
+		print(r"%d/%d"%(i,tasks), future)
